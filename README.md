39ea14e655face22db19dc881ce3d702edb43cd3
cyrraz <code.cyrraz@protonmail.com>
2024-01-24 14:22:50 +0100

    add faster example

--- README.md
+++ README.md
@@ -3,8 +3,13 @@ Example showing how to visualise ~20GB of data using [plothist](https://plothist
 The data are generated/loaded in batches so that the required memory is only ~2GB.
 
 * `generate_data.py` generates a data file `data.pq` of ~20GB using `plothist` (takes ~3min).
-* `plot_data.py` plots the generated data using `pyarrow` and `plothist` (takes ~1min).
+* `plot_data.py` plots the generated data using `pyarrow` and `plothist` (takes ~30s).
+* `plot_categorised_weighted_data.py` plots the generated data using `pyarrow` and `plothist` taking into account different data categories and a specific weight for each entry (takes ~1min).
 
-This is the resulting plot:
+This is the resulting plot of `plot_data.py`:
 
 ![variable.svg](https://raw.githubusercontent.com/cyrraz/visualise-large-dataset/main/variable.svg)
+
+And the result of `plot_categorised_weighted_data.py`:
+
+![variable_categorised_weighted.svg](https://raw.githubusercontent.com/cyrraz/visualise-large-dataset/main/variable_categorised_weighted.svg)
--- plot_data.py
+++ plot_data.py
@@ -8,32 +8,19 @@ batch_size = int(1e7)
 range_ = (0, 6)
 bins = 50
 
-hist_normal = make_hist(data=[], range=range_, bins=bins)
-hist_exponential = make_hist(data=[], range=range_, bins=bins)
-hist_log_normal = make_hist(data=[], range=range_, bins=bins)
+hist = make_hist(data=[], range=range_, bins=bins)
 
-for batch in parquet_file.iter_batches(batch_size=batch_size):
-    variable = batch.column("variable").to_pandas(zero_copy_only=True).values
-    category = batch.column("category").to_pandas(zero_copy_only=True).values
-    weight = batch.column("weight").to_pandas(zero_copy_only=True).values
-
-    hist_normal.fill(variable[category == 0], weight=weight[category == 0])
-    hist_exponential.fill(variable[category == 1], weight=weight[category == 1])
-    hist_log_normal.fill(variable[category == 2], weight=weight[category == 2])
+for batch in parquet_file.iter_batches(batch_size=batch_size, columns=["variable"]):
+    hist.fill(batch.column("variable"), threads=0)
 
 fig, ax = plt.subplots()
 
-plot_hist(
-    [hist_normal, hist_exponential, hist_log_normal],
-    ax=ax,
-    stacked=True,
-    label=["Gaussian", "Exponential", "Log-normal"],
-)
+plot_hist(hist, ax=ax, label="Data")
 
 ax.set_xlabel("Variable")
 ax.set_ylabel("Entries")
 ax.set_xlim(range_)
 
-ax.legend(reverse=True)
+ax.legend()
 
 fig.savefig("variable.svg", bbox_inches="tight")
