1f93ec5f217099fe08101b2150e71a2d6c93f7cc
cyrraz <code.cyrraz@protonmail.com>
2024-01-24 12:01:47 +0100

    variable renaming

--- generate_data.py
+++ generate_data.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 filename = "data2.pq"
 total_size = int(2e10)
-chunk_size = int(1e7)
+batch_size = int(1e7)
 
 # Define schema for the Parquet file
 schema = pa.schema(
@@ -23,19 +23,19 @@ with pq.ParquetWriter(filename, schema) as writer:
     while file_path.stat().st_size < total_size:
         array_variable = np.concatenate(
             [
-                np.random.normal(loc=3, size=chunk_size),
-                np.random.exponential(size=chunk_size),
-                np.random.lognormal(size=chunk_size),
+                np.random.normal(loc=3, size=batch_size),
+                np.random.exponential(size=batch_size),
+                np.random.lognormal(size=batch_size),
             ]
         )
         array_category = np.concatenate(
             [
-                np.zeros(chunk_size, dtype=np.int8),
-                np.ones(chunk_size, dtype=np.int8),
-                2 * np.ones(chunk_size, dtype=np.int8),
+                np.zeros(batch_size, dtype=np.int8),
+                np.ones(batch_size, dtype=np.int8),
+                2 * np.ones(batch_size, dtype=np.int8),
             ]
         )
-        array_weight = np.random.poisson(size=3 * chunk_size)
+        array_weight = np.random.poisson(size=3 * batch_size)
 
         # Convert the NumPy array to a PyArrow Table
         table = pa.Table.from_arrays(
