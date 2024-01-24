e3a8634020934aae8aa0e5ff9cf3702bc18cdb4b
cyrraz <code.cyrraz@protonmail.com>
2024-01-24 12:00:23 +0100

    fix code style

--- generate_data.py
+++ generate_data.py
@@ -8,7 +8,9 @@ total_size = int(2e10)
 chunk_size = int(1e7)
 
 # Define schema for the Parquet file
-schema = pa.schema([("variable", pa.float64()), ("category", pa.int8()), ("weight", pa.int64())])
+schema = pa.schema(
+    [("variable", pa.float64()), ("category", pa.int8()), ("weight", pa.int64())]
+)
 
 
 # Check if the file already exists
@@ -19,7 +21,6 @@ if file_path.exists():
 
 with pq.ParquetWriter(filename, schema) as writer:
     while file_path.stat().st_size < total_size:
-
         array_variable = np.concatenate(
             [
                 np.random.normal(loc=3, size=chunk_size),
@@ -31,18 +32,18 @@ with pq.ParquetWriter(filename, schema) as writer:
             [
                 np.zeros(chunk_size, dtype=np.int8),
                 np.ones(chunk_size, dtype=np.int8),
-                2*np.ones(chunk_size, dtype=np.int8),
+                2 * np.ones(chunk_size, dtype=np.int8),
             ]
         )
-        array_weight = np.random.poisson(size=3*chunk_size)
+        array_weight = np.random.poisson(size=3 * chunk_size)
 
         # Convert the NumPy array to a PyArrow Table
         table = pa.Table.from_arrays(
             [array_variable, array_category, array_weight],
             names=["variable", "category", "weight"],
         )
-        
+
         # Save the Table as a Parquet file
         writer.write_table(table)
-        
+
 print(f"Created a Parquet file.")
--- plot_data.py
+++ plot_data.py
@@ -2,10 +2,10 @@ import pyarrow.parquet as pq
 from plothist import make_hist, plot_hist
 import matplotlib.pyplot as plt
 
-parquet_file = pq.ParquetFile('data.pq')
+parquet_file = pq.ParquetFile("data.pq")
 batch_size = int(1e7)
 
-range_ = (0,6)
+range_ = (0, 6)
 bins = 50
 
 hist_normal = make_hist(data=[], range=range_, bins=bins)
@@ -13,18 +13,22 @@ hist_exponential = make_hist(data=[], range=range_, bins=bins)
 hist_log_normal = make_hist(data=[], range=range_, bins=bins)
 
 for batch in parquet_file.iter_batches(batch_size=batch_size):
-
     variable = batch.column("variable").to_pandas(zero_copy_only=True).values
     category = batch.column("category").to_pandas(zero_copy_only=True).values
     weight = batch.column("weight").to_pandas(zero_copy_only=True).values
 
-    hist_normal.fill(variable[category==0], weight=weight[category==0])
-    hist_exponential.fill(variable[category==1], weight=weight[category==1])
-    hist_log_normal.fill(variable[category==2], weight=weight[category==2])
+    hist_normal.fill(variable[category == 0], weight=weight[category == 0])
+    hist_exponential.fill(variable[category == 1], weight=weight[category == 1])
+    hist_log_normal.fill(variable[category == 2], weight=weight[category == 2])
 
 fig, ax = plt.subplots()
 
-plot_hist([hist_normal, hist_exponential, hist_log_normal], ax=ax, stacked=True, label=["Gaussian", "Exponential", "Log-normal"])
+plot_hist(
+    [hist_normal, hist_exponential, hist_log_normal],
+    ax=ax,
+    stacked=True,
+    label=["Gaussian", "Exponential", "Log-normal"],
+)
 
 ax.set_xlabel("Variable")
 ax.set_ylabel("Entries")
